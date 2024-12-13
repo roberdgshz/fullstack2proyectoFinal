6647e2a711477229fc0c2b8cfe0478a79fa06cf5
Roberto Grijalva Sánchez <roberd.999117@gmail.com>
2024-12-13 16:51:16 -0600

    Solución al /admin de Docker

--- paginas/urls.py
+++ paginas/urls.py
@@ -3,6 +3,4 @@ from .views import * # VistaBase, VistaEncabezado
 
 urlpatterns = [
     path('', VistaInicio.as_view(), name="inicio"),
-    path('base',VistaBase.as_view(), name="base"),
-    path('enc',VistaEncabezado.as_view(), name="enc")
 ]
--- paginas/views.py
+++ paginas/views.py
@@ -1,10 +1,4 @@
 from django.views.generic import TemplateView
 
-class VistaBase(TemplateView):
-    template_name = 'base.html'
-
-class VistaEncabezado(TemplateView):
-    template_name = 'encabezado.html'
-
 class VistaInicio(TemplateView):
     template_name = 'inicio.html'
